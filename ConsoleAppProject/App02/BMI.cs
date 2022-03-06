97d731b78b2d955ca6fd240969144b37d1045948
Muhammad Shakeel <muhammadshakeel9995@gmail.com>
2022-03-06 17:21:18 +0000

    screenshots added

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// This class contains methods for calculating 
-    /// the user's BMI (Body Mass Index) using 
+    /// the user's BMI (Body Mass value) using 
     /// imperial or metric units.
     /// </summary>
     /// <Author>
@@ -22,11 +22,13 @@ namespace ConsoleAppProject.App02
             int choice = ConsoleHelper.SelectChoice(choices);
             if (choice == 1)
             {
-                MetricCalculator();
+                double value = MetricCalculator();
+                Console.WriteLine("Your BMI is " + value);
+                HelthMessage(value);
             }
             else if (choice == 2)
             {
-
+                ImperialCalculator();
 
             }
             else
@@ -39,7 +41,7 @@ namespace ConsoleAppProject.App02
 
 
 
-        public void MetricCalculator()
+        public double MetricCalculator()
             {
 
            int Centimetres = (int)ConsoleHelper.InputNumber(" \n Enter your height in centimetres > ");
@@ -49,16 +51,78 @@ namespace ConsoleAppProject.App02
            int Kilograms = (int)ConsoleHelper.InputNumber(" Enter your weight in kilograms > ");
 
             double value = Kilograms / (metres * metres);
-            Console.WriteLine("Your BMI is "+value);
+            return value;
         }
+
+
+
+        public void ImperialCalculator()
+        {
+            Console.WriteLine(" Enter your height to the nearest feet and inches ");
+            Console.WriteLine();
+
+            int Feet = (int)ConsoleHelper.InputNumber("\n Enter your height in feet > ");
+            int Inches = (int)ConsoleHelper.InputNumber(" Enter your height in inches > ");
+
+            Console.WriteLine();
+            Console.Write(" Enter your weight to the nearest stones and pounds");
+            Console.WriteLine();
+
+            int Stones = (int)ConsoleHelper.InputNumber(" Enter your weight in stones > ");
+            int Pounds = (int)ConsoleHelper.InputNumber(" Enter your weight in pounds > ");
+
+            Inches += Feet * 12;
+            Pounds += Stones * 14;
+
+            double value = (double)Pounds * 703 / (Inches * Inches);
+
+            Console.WriteLine("Your BMI is " + value);
+            HelthMessage(value);
+        }
+
+
+
+        public void HelthMessage(double value)
+        {
+            if (value < 18.5)
+            {
+                Console.WriteLine("You are underweight! ");
+            }
+            else if (value <= 24.9)
+            {
+                Console.WriteLine("You are in the normal range! ");
+
+            }
+            else if (value <= 29.9)
+            {
+                Console.WriteLine("You are overweight! ");
+
+            }
+            else if (value <= 34.9)
+            {
+                Console.WriteLine("You are obese class I ");
+
+            }
+            else if (value <= 39.9)
+            {
+                Console.WriteLine("You are obese class II ");
+
+            }
+            else if (value <= 40)
+            {
+                Console.WriteLine("You are obese class III ");
+
+            }
+
+        }
+
 
         public void BMIMessage()
         {
-            Console.WriteLine(" If you are Black, Asian or other minority");
-            Console.WriteLine("\n ethnic groups, you have a higher risk");
-            Console.WriteLine("\n");
-            Console.WriteLine("\n Adults 23.0 or more are at increased risk");
-            Console.WriteLine("\n Adults 27.5 or more are at high risk");
+            Console.WriteLine("If you are Black, Asian or other minority");
+            Console.WriteLine("ethnic groups, you have a higher risk");
+            Console.WriteLine("Adults 23.0 or more are at increased risk");
+            Console.WriteLine("Adults 27.5 or more are at high risk");
             Console.WriteLine("\n");
         }
 
