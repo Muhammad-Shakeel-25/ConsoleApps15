3868f6aada796e8999f7cd5dd26537a9f33efd9c
Muhammad Shakeel <muhammadshakeel9995@gmail.com>
2022-04-03 15:28:58 +0100

    updated

--- ConsoleAppProject/App04/SocialNetwork.cs
+++ ConsoleAppProject/App04/SocialNetwork.cs
@@ -10,6 +10,7 @@ namespace ConsoleAppProject.App04
 
         string[] post = new string[99] ;
         string[] author = new string[99];
+        string[] comment = new string[99];
         int[] like = new int[99];
         int id = 0;
 
@@ -32,6 +33,10 @@ namespace ConsoleAppProject.App04
             {
                 PostMessage();
             }
+            else if (choice == 2)
+            {
+                PostPhoto();
+            }
             else if (choice == 3)
             {
                 RemovePost();
@@ -39,6 +44,14 @@ namespace ConsoleAppProject.App04
             else if(choice == 4)
             {
                 DisplayAllPosts();
+            }
+            else if (choice == 5)
+            {
+                DisplayPostsByAuthor();
+            }
+            else if (choice == 6)
+            {
+                AddComment();
             }
             else if (choice == 7)
             {
@@ -48,6 +61,80 @@ namespace ConsoleAppProject.App04
             {
                 UnLikePost();
             }
+        }
+
+        private void PostMessage()
+        {
+            Console.WriteLine(" Please enter author Name:");
+            author[id] = Console.ReadLine();
+            Console.WriteLine(" Please enter post message:");
+            post[id] = Console.ReadLine();
+            like[id] = 0;
+            id++;
+
+            ReRun();
+
+        }
+
+        private void PostPhoto()
+        {
+            Console.WriteLine(" Please enter author Name:");
+            author[id] = Console.ReadLine();
+            Console.WriteLine(" Please enter post photo url:");
+            post[id] = Console.ReadLine();
+            like[id] = 0;
+            id++;
+
+            ReRun();
+        }
+
+        private void AddComment()
+        {
+            Console.WriteLine(" Please Enter Post ID>");
+            int postid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Please Enter Your Comment>");
+            comment[postid] = " Comment for Post id "+postid+") "+Console.ReadLine();
+            Console.WriteLine(" Comment added to post id " + postid);
+            ReRun();
+        }
+
+        private void showComments(int postID)
+        {
+            for(int i=0;i<comment.Length;i++)
+            {
+                if(comment[i]!=null)
+                { 
+                    if(comment[i].Contains(postID+")"))
+                Console.WriteLine(" " + comment[i]);
+                }
+            }
+        }
+
+        private void DisplayPostsByAuthor()
+        {
+            Console.WriteLine(" Please enter the name of author>");
+            String authorName = Console.ReadLine();
+            for (int i = 0; i < id; i++)
+            {
+                if (post[i] != "" && author[i]==authorName)
+                {
+                    Console.WriteLine(" Post ID " + i + " | Post Author " + author[i]);
+                    Console.WriteLine(" " + post[i]);
+                    Console.WriteLine(" Likes " + like[i]);
+                    showComments(i);
+                }
+            }
+
+
+            ReRun();
+
+        }
+
+        private void ReRun()
+        {
+            Console.WriteLine(" Want to show ALL OPTIONS AGAIN? y/n");
+            if (Console.ReadLine() == "y")
+                Run();
         }
 
         private void UnLikePost()
@@ -63,7 +150,7 @@ namespace ConsoleAppProject.App04
             {
                 Console.WriteLine(" Post already unliked ");
             }
-            Run();
+            ReRun();
 
 
         }
@@ -74,7 +161,7 @@ namespace ConsoleAppProject.App04
             int postid = Convert.ToInt32(Console.ReadLine());
             like[postid] = like[postid]+1;
             Console.WriteLine(" Post Liked with ID " + postid);
-            Run();
+            ReRun();
         }
 
         private void RemovePost()
@@ -83,7 +170,7 @@ namespace ConsoleAppProject.App04
             int postid = Convert.ToInt32(Console.ReadLine());
             post[postid] = "";
             Console.WriteLine(" Post Removed with ID " + postid);
-            Run();
+            ReRun();
         }
 
         private void DisplayAllPosts()
@@ -95,23 +182,13 @@ namespace ConsoleAppProject.App04
                 Console.WriteLine(" Post ID " + i +" | Post Author " + author[i]);
                 Console.WriteLine(" "+post[i]);
                 Console.WriteLine(" Likes "+like[i]);
+                    showComments(i);
                 }
             }
 
-            Run();
+            ReRun();
         }
 
-        private void PostMessage()
-        {
-            Console.WriteLine(" Please enter author Name:");
-            author[id] = Console.ReadLine();
-            Console.WriteLine(" Please enter post message:");
-            post[id] = Console.ReadLine();
-            like[id] = 0;
-            id++;
-
-            Run();
-
-        }
+       
     }
 }
